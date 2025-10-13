feca46cf48446d119113ae6e491d36641df69f14
tufail101 <modreneducation479@gmail.com>
2025-10-14 01:27:33 +0530

    adding login and register

--- login.js
+++ login.js
@@ -2,10 +2,12 @@ document
   .getElementById("loginForm")
   .addEventListener("submit", async function (e) {
     e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPassword").value;
 
     try {
+      console.log(email, password);
+      
       const res = await axios.post(
         `${BASE_URL_USER}/login`,
         {
@@ -18,13 +20,13 @@ document
 
       if (res.data.statusCode === 200) {
         localStorage.setItem("user",res.data)
-        showToast("Login successful!", true);
+        // showToast("Login successful!", true);
         window.location.href = "./index.html";
       }
     } catch (error) {
       console.error(error);
       const message = error.response?.data?.message || "Login failed!";
-      showToast(message, false);
+      // showToast(message, false);
     }
   });
 
@@ -40,11 +42,18 @@ if (registerForm) {
     const name = document.getElementById("name").value
     const email = document.getElementById("email").value
     const password = document.getElementById("password").value
+    const role = document.getElementById("role").value
+    const department = document.getElementById("department").value
 
 
     try {
-        const res = await axios.post(`${BASE_URL_USER}/register`,{name, email,password},{withCredentials : true})
+        const res = await axios.post(`${BASE_URL_USER}/register`,{name, email,password,role,department},{withCredentials : true})
         console.log(res.data);
+        if (res.data.statusCode === 201) {
+          setTimeout(() => {
+            window.location.reload()
+          },1000)
+        }
         
     } catch (error) {
         console.log(error);
